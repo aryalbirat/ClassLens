cd92bf24f4dbf6de54fb7f448b599d9405744c73
Birat <itsbirataryal+github@gmail.com>
2024-11-29 20:20:15 +0545

    Added some transitions

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className="flex flex-col  bg-gradient-to-r from-[#2b3035] to-[#14176d] shadow-sm border border-slate-200 rounded-lg my-7 w-60">
+        <div className="flex flex-col transition delay-50 hover:-translate-y-5 bg-gradient-to-r from-[#2b3035] to-[#14176d] shadow-sm border border-slate-200 rounded-lg my-7 w-60">
             <div className="m-4 overflow-hidden rounded-md h-25 flex justify-center items-center">
                 <img 
                     className="w-full h-64 object-cover overflow-hidden" 
