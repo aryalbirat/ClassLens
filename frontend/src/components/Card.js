263d4b623e47c1e3b1dd0279f5a3b7f0ac79eab5
Rasad Regmi <regmirasad53@gmail.com>
2024-11-29 19:55:19 +0545

    Made some ui changes

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React from "react";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
 
-const Card = (props) => {
-    return (
-        <div className="flex flex-col bg-slate-800 shadow-sm border border-slate-200 rounded-lg my-7 w-60">
-            <div className="m-4 overflow-hidden rounded-md h-25 flex justify-center items-center">
-                <img 
-                    className="w-full h-64 object-cover overflow-hidden" 
-                    src={props.src} 
-                    alt={`${props.name}'s profile`} 
-                />
-            </div>
-            <div className="p-6 text-center">
-                <h4 className="mb-1 text-xl font-semibold">
-                    {props.name}
-                </h4>
-            </div>
-            <div className="flex justify-center p-6 pt-2 gap-3">
-                <button className="relative flex justify-center items-center w-[150px] h-[40px] p-[10px] gap-[8px] bg-gradient-to-r from-[#7732FF] to-[#8862c2] border border-[#2C2C2C] rounded-lg text-white text-[18px] font-normal leading-none shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:opacity-80 hover:cursor-pointer">
-                    Follow
-                </button>
-            </div>
+const Card = ({ src, name, facebook, linkedin, github }) => {
+  return (
+    <div className="flex flex-col bg-slate-800 shadow-sm border border-slate-200 rounded-lg my-7 w-60">
+      <div className="m-4 overflow-hidden rounded-md h-25 flex justify-center items-center">
+        <img
+          className="w-full h-64 object-cover overflow-hidden"
+          src={src}
+          alt={`${name}'s profile`}
+        />
+      </div>
+      <div className="p-6 text-center">
+        <h4 className="mb-1 text-xl font-semibold">{name}</h4>
+      </div>
+      <div className="flex justify-center p-8 pt-2 gap-3">
+        <div className="flex gap-4">
+          {facebook && (
+            <a href={facebook} target="_blank" rel="noopener noreferrer">
+              <span className="bannerIcon">
+                <FaFacebookF />
+              </span>
+            </a>
+          )}
+          {linkedin && (
+            <a href={linkedin} target="_blank" rel="noopener noreferrer">
+              <span className="bannerIcon">
+                <FaLinkedinIn />
+              </span>
+            </a>
+          )}
+          {github && (
+            <a href={github} target="_blank" rel="noopener noreferrer">
+              <span className="bannerIcon">
+                <BsGithub />
+              </span>
+            </a>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- frontend/src/pages/Team.js
+++ frontend/src/pages/Team.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Card from '../components/Card';
-import biratImage from '../assets/birat.jpg';
-import dipeshImage from '../assets/dipesh.jpg';
-import pranavImage from '../assets/pranav.jpg';
-import rasadImage from '../assets/rasad.jpg';
+import React from "react";
+import Card from "../components/Card";
+import biratImage from "../assets/birat.jpg";
+import dipeshImage from "../assets/dipesh.jpg";
+import pranavImage from "../assets/pranav.jpg";
+import rasadImage from "../assets/rasad.jpg";
 
 const Team = () => {
   return (
@@ -19,10 +19,34 @@ const Team = () => {
 
         <div className="mt-8">
           <div className="flex gap-5 flex-wrap justify-center">
-            <Card name="Rasad Regmi" src={rasadImage} />
-            <Card name="Birat Aryal" src={biratImage} />
-            <Card name="Dipesh D.C." src={dipeshImage} />
-            <Card name="Pranav Subedi" src={pranavImage} />
+            <Card
+              name="Rasad Regmi"
+              src={rasadImage}
+              facebook="https://www.facebook.com/rasad.regmi"
+              linkedin="https://www.linkedin.com/in/rasad-regmi/"
+              github="https://github.com/rasadregmi"
+            />
+            <Card
+              name="Birat Aryal"
+              src={biratImage}
+              facebook="https://www.facebook.com/aryalbiratt"
+              linkedin="https://www.linkedin.com/in/aryalbirat/"
+              github="https://github.com/aryalbirat"
+            />
+            <Card
+              name="Dipesh D.C."
+              src={dipeshImage}
+              facebook="https://www.facebook.com/profile.php?id=100088129415264"
+              linkedin="https://www.linkedin.com/in/dipesh-dc-25a140248/"
+              github="https://github.com/dipeshdc"
+            />
+            <Card
+              name="Pranav Subedi"
+              src={pranavImage}
+              facebook="https://www.facebook.com/pranav.subdee"
+              linkedin="https://www.linkedin.com/in/pranav-subedi-132261322/"
+              github="https://github.com/pranavsubedi12"
+            />
           </div>
         </div>
       </div>
