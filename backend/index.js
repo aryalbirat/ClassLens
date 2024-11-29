a66b667fee73249831545348c9170ad9b26f723f
dipeshdc <dipeshdc12@gmail.com>
2024-11-29 10:32:36 +0545

    Backend connected with ML api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const multer  = require('multer')
 const cors = require("cors")
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
 
 const app=express()
 app.use(cors())
 app.use(express.json())
+
+const mlUrl = 'http://localhost:8000/predict'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,10 +23,22 @@ const storage = multer.diskStorage({
   const upload = multer({ storage })
 
   app.post("/single",upload.single("image") ,(req, res)=> {
-    console.log(req.file.filename)
-    res.json({
-        message: "The image has been received"
-    });
+    const filename = req.file.filename
+    const filepath = `./uploads/${filename}`
+
+    const form = new FormData();
+    form.append('file', fs.createReadStream(filepath));
+
+    axios.post(mlUrl , form, {
+      headers: form.getHeaders(),
+    })
+      .then(response => {
+        res.status(200).json(response.data);
+      })
+      .catch(error => {
+        console.error(error.response?.data || error.message);
+        res.status(500).json({ error: 'Error processing image in ML API' });
+      });
   })
 
 app.listen(8080,() =>{
